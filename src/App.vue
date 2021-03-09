513009af03542c8b59dd7be090def19a5e33f933
aakash2809 <aakashrajak289@gmail.com>
2021-03-09 10:20:52 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!--    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <!-- <router-view /> -->
+    <SignUp />
+  </div>
+</template>
+
+<script>
+/* import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+} */
+
+import SignUp from "./components/SignUp";
+export default {
+  name: "App",
+  components: {
+    SignUp,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div class="md-layout">
+      <div class="md-layout-item"></div>
+      <div class="md-layout-item"></div>
+      <div class="md-layout-item">
+        <h5>Create Account?</h5>
+      </div>
+      <div class="md-layout-item">
+        <md-button class="md-raised" @click="goToRegisterPage"
+          >Sign up</md-button
+        >
+      </div>
+      <div class="md-layout-item"></div>
+      <div class="md-layout-item"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import router from "../router";
+export default {
+  name: "home",
+  methods: {
+    goToRegisterPage() {
+      router.push({ name: "Register" });
+    },
+  },
+};
+</script>
+
