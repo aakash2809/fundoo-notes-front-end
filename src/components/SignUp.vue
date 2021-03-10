f1ec0cc05e765793f33e29ab1bdd0654ff16618f
aakash2809 <aakashrajak289@gmail.com>
2021-03-10 07:59:55 +0530

    refactor : update view of signUp

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,178 +1,162 @@
 <template>
-  <div>
-    <div id="logo" class="v8vQje" title="FundooNotes">
-      <div class="rr0DNb" jsname="l4eHX">
-        <svg
-          viewBox="0 0 75 24"
-          width="75"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true"
-          class="l5Lhkf"
-        >
-          <g id="qaEJec">
-            <path
-              fill="#ea4335"
-              d="M67.954 16.303c-1.33 0-2.278-.608-2.886-1.804l7.967-3.3-.27-.68c-.495-1.33-2.008-3.79-5.102-3.79-3.068 0-5.622 2.41-5.622 5.96 0 3.34 2.53 5.96 5.92 5.96 2.73 0 4.31-1.67 4.97-2.64l-2.03-1.35c-.673.98-1.6 1.64-2.93 1.64zm-.203-7.27c1.04 0 1.92.52 2.21 1.264l-5.32 2.21c-.06-2.3 1.79-3.474 3.12-3.474z"
-            ></path>
-          </g>
-          <g id="YGlOvc">
-            <path fill="#34a853" d="M58.193.67h2.564v17.44h-2.564z"></path>
-          </g>
-          <g id="BWfIk">
-            <path
-              fill="#4285f4"
-              d="M54.152 8.066h-.088c-.588-.697-1.716-1.33-3.136-1.33-2.98 0-5.71 2.614-5.71 5.98 0 3.338 2.73 5.933 5.71 5.933 1.42 0 2.548-.64 3.136-1.36h.088v.86c0 2.28-1.217 3.5-3.183 3.5-1.61 0-2.6-1.15-3-2.12l-2.28.94c.65 1.58 2.39 3.52 5.28 3.52 3.06 0 5.66-1.807 5.66-6.206V7.21h-2.48v.858zm-3.006 8.237c-1.804 0-3.318-1.513-3.318-3.588 0-2.1 1.514-3.635 3.318-3.635 1.784 0 3.183 1.534 3.183 3.635 0 2.075-1.4 3.588-3.19 3.588z"
-            ></path>
-          </g>
-          <g id="e6m3fd">
-            <path
-              fill="#fbbc05"
-              d="M38.17 6.735c-3.28 0-5.953 2.506-5.953 5.96 0 3.432 2.673 5.96 5.954 5.96 3.29 0 5.96-2.528 5.96-5.96 0-3.46-2.67-5.96-5.95-5.96zm0 9.568c-1.798 0-3.348-1.487-3.348-3.61 0-2.14 1.55-3.608 3.35-3.608s3.348 1.467 3.348 3.61c0 2.116-1.55 3.608-3.35 3.608z"
-            ></path>
-          </g>
-          <g id="vbkDmc">
-            <path
-              fill="#ea4335"
-              d="M25.17 6.71c-3.28 0-5.954 2.505-5.954 5.958 0 3.433 2.673 5.96 5.954 5.96 3.282 0 5.955-2.527 5.955-5.96 0-3.453-2.673-5.96-5.955-5.96zm0 9.567c-1.8 0-3.35-1.487-3.35-3.61 0-2.14 1.55-3.608 3.35-3.608s3.35 1.46 3.35 3.6c0 2.12-1.55 3.61-3.35 3.61z"
-            ></path>
-          </g>
-          <g id="idEJde">
-            <path
-              fill="#4285f4"
-              d="M14.11 14.182c.722-.723 1.205-1.78 1.387-3.334H9.423V8.373h8.518c.09.452.16 1.07.16 1.664 0 1.903-.52 4.26-2.19 5.934-1.63 1.7-3.71 2.61-6.48 2.61-5.12 0-9.42-4.17-9.42-9.29C0 4.17 4.31 0 9.43 0c2.83 0 4.843 1.108 6.362 2.56L14 4.347c-1.087-1.02-2.56-1.81-4.577-1.81-3.74 0-6.662 3.01-6.662 6.75s2.93 6.75 6.67 6.75c2.43 0 3.81-.972 4.69-1.856z"
-            ></path>
-          </g>
-        </svg>
-      </div>
-    </div>
-
+  <div class="container">
     <form novalidate class="md-layout jc-center" @submit.prevent="validateUser">
-      <md-card class="md-layout-item md-size-50 md-small-size-500 overflow-x">
-        <md-card-header>
-          <div class="md-title">Create Your Account</div>
-        </md-card-header>
+      <md-card class="md-layout-item md-size-90 md-small-size-100">
+        <div class="md-layout md-gutter">
+          <div class="md-layout-item md-small-size-100">
+            <md-card-header>
+              <div class="md-title">
+                <Title />
+                Create your Account
+              </div>
+            </md-card-header>
 
-        <md-card-content>
-          <div class="md-layout md-gutter">
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('firstName')">
-                <label for="first-name">First Name</label>
+            <md-card-content>
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('firstName')">
+                    <label for="last-name">Last Name</label>
+                    <md-input
+                      id="first-name"
+                      v-model="form.firstName"
+                      label="First name"
+                      :disabled="sending"
+                    />
+
+                    <span class="md-error" v-if="!$v.form.firstName.required"
+                      >The first name is required</span
+                    >
+                    <span
+                      class="md-error"
+                      v-else-if="!$v.form.firstName.minlength"
+                      >Invalid first name</span
+                    >
+                  </md-field>
+                </div>
+
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('lastName')">
+                    <label for="last-name">Last Name</label>
+                    <md-input
+                      name="last-name"
+                      id="last-name"
+                      autocomplete="family-name"
+                      v-model="form.lastName"
+                      outline
+                      dense
+                      :disabled="sending"
+                    />
+                    <span class="md-error" v-if="!$v.form.lastName.required"
+                      >The last name is required</span
+                    >
+                    <span
+                      class="md-error"
+                      v-else-if="!$v.form.lastName.minlength"
+                      >Invalid last name</span
+                    >
+                  </md-field>
+                </div>
+              </div>
+
+              <md-field :class="getValidationClass('email')">
+                <label for="email">email</label>
                 <md-input
-                  name="first-name"
-                  id="first-name"
-                  autocomplete="given-name"
-                  v-model="form.firstName"
+                  outline
+                  dense
+                  type="email"
+                  name="email"
+                  id="email"
+                  autocomplete="email"
+                  v-model="form.email"
                   :disabled="sending"
-                  v
                 />
-                <span class="md-error" v-if="!$v.form.firstName.required"
-                  >The first name is required</span
+                <span class="md-error" v-if="!$v.form.email.required"
+                  >The email is required</span
                 >
-                <span class="md-error" v-else-if="!$v.form.firstName.minlength"
-                  >Invalid first name</span
+                <span class="md-error" v-else-if="!$v.form.email.email"
+                  >Invalid email</span
                 >
               </md-field>
-            </div>
 
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('lastName')">
-                <label for="last-name">Last Name</label>
-                <md-input
-                  name="last-name"
-                  id="last-name"
-                  autocomplete="family-name"
-                  v-model="form.lastName"
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('password')">
+                    <label for="password">Password</label>
+                    <md-input
+                      name="password"
+                      type="password"
+                      id="password"
+                      v-model="form.password"
+                      :disabled="sending"
+                    />
+                    <span class="md-error" v-if="!$v.form.password.required"
+                      >The password is required</span
+                    >
+                    <span
+                      class="md-error"
+                      v-else-if="!$v.form.password.minlength"
+                      >Password should contain minimum 4 charecters
+                    </span>
+                  </md-field>
+                </div>
+
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('password')">
+                    <label for="cpassword">Confirm Password</label>
+                    <md-input
+                      name="cpassword"
+                      type="password"
+                      id="cpassword"
+                      v-model="form.cpassword"
+                      :disabled="sending"
+                    />
+                    <span class="md-error" v-if="!$v.form.cpassword.required"
+                      >The confirm password is required</span
+                    >
+                    <span
+                      class="md-error"
+                      v-else-if="!$v.form.cpassword.minlength"
+                      >Confirm Password should contain minimum 4 charecters
+                    </span>
+                  </md-field>
+                </div>
+              </div>
+            </md-card-content>
+            <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+            <md-card-content>
+              <md-card-actions>
+                <span class="sign-in" style="flex: 1">sign in instead</span>
+                <v-spacer> </v-spacer>
+                <md-button
+                  type="submit"
+                  class="md-raised md-primary"
                   :disabled="sending"
-                />
-                <span class="md-error" v-if="!$v.form.lastName.required"
-                  >The last name is required</span
+                  >Next</md-button
                 >
-                <span class="md-error" v-else-if="!$v.form.lastName.minlength"
-                  >Invalid last name</span
-                >
-              </md-field>
-            </div>
+              </md-card-actions>
+            </md-card-content>
           </div>
-          <md-field :class="getValidationClass('email')">
-            <label for="email">Email</label>
-            <md-input
-              type="email"
-              name="email"
-              id="email"
-              autocomplete="email"
-              v-model="form.email"
-              :disabled="sending"
-            />
-            <span class="md-error" v-if="!$v.form.email.required"
-              >The email is required</span
-            >
-            <span class="md-error" v-else-if="!$v.form.email.email"
-              >Invalid email</span
-            >
-          </md-field>
-
-          <div class="md-layout md-gutter">
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('password')">
-                <label for="password">Password</label>
-                <md-input
-                  name="password"
-                  type="password"
-                  id="password"
-                  v-model="form.password"
-                  :disabled="sending"
-                />
-                <span class="md-error" v-if="!$v.form.password.required"
-                  >The password is required</span
-                >
-                <span class="md-error" v-else-if="!$v.form.password.minlength"
-                  >Password should contain minimum 4 charecters
-                </span>
-              </md-field>
-            </div>
-
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('password')">
-                <label for="cpassword">Confirm Password</label>
-                <md-input
-                  name="cpassword"
-                  type="password"
-                  id="cpassword"
-                  v-model="form.cpassword"
-                  :disabled="sending"
-                />
-                <span class="md-error" v-if="!$v.form.cpassword.required"
-                  >The confirm password is required</span
-                >
-                <span class="md-error" v-else-if="!$v.form.cpassword.minlength"
-                  >Confirm Password should contain minimum 4 charecters
-                </span>
-              </md-field>
-            </div>
+          <div>
+            <figure class="account-img">
+              <img
+                src="https://ssl.gstatic.com/accounts/signup/glif/account.svg"
+                alt=""
+                width="244"
+                height="244"
+                class="j9NuTc TrZEUc"
+              />
+              <figcaption class="oEvHdd">
+                One account. All of Fundoo working for you.
+              </figcaption>
+            </figure>
           </div>
-        </md-card-content>
-
-        <md-progress-bar md-mode="indeterminate" v-if="sending" />
-
-        <md-card-actions>
-          <md-button type="submit" class="md-primary" :disabled="sending"
-            >Create user</md-button
-          >
-        </md-card-actions>
+        </div>
       </md-card>
-      <figure class="oFqWT">
-        <img
-          src="https://ssl.gstatic.com/accounts/signup/glif/account.svg"
-          alt=""
-          width="244"
-          height="244"
-          class="j9NuTc TrZEUc"
-        />
-        <figcaption class="oEvHdd">
-          One account. All of Google working for you.
-        </figcaption>
-      </figure>
+
+      <md-snackbar :md-active.sync="userSaved"
+        >The user {{ lastUser }} was saved with success!</md-snackbar
+      >
     </form>
   </div>
 </template>
@@ -181,6 +165,8 @@
 import { validationMixin } from "vuelidate";
 import router from "../router";
 import { required, email } from "vuelidate/lib/validators";
+import Title from "./title";
+
 export default {
   name: "SignUp",
   mixins: [validationMixin],
@@ -238,7 +224,9 @@ export default {
       this.form.password = null;
       this.form.cpassword = null;
       window.setTimeout(() => {
-        router.push({ name: "home" });
+        router.push({
+          name: "home",
+        });
       }, 2000);
     },
     saveUser() {
@@ -260,6 +248,9 @@ export default {
       }
     },
   },
+  components: {
+    Title,
+  },
 };
 </script>
 
@@ -274,16 +265,30 @@ export default {
 .jc-center {
   justify-content: center;
 }
+
 .overflow-x {
   overflow-x: hidden;
 }
 
-/* .side-logo {
-  padding: 5px;
-  background-color: cornflowerblue;
-}*/
-.image-combo {
-  padding: 0px;
-  margin: 0px;
+.md-primary {
+  background-color: blue;
 }
+
+.sign-in {
+  padding-right: 600px;
+  margin-left: -26px;
+  color: blue;
+}
+.fundoo-title {
+  padding-left: -200px;
+  margin-left: 100px;
+}
+/* .container {
+  display: inline-block;
+  font-size: 14px;
+  padding: 24px 0 0;
+  vertical-align: top;
+  white-space: normal;
+  width: 100%;
+} */
 </style>
---
+++ src/components/title.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-card-title>
+    <span class="md-title-col1">F</span>
+    <span class="md-title-col2">u</span>
+    <span class="md-title-col3">n</span>
+    <span class="md-title-col4">d</span>
+    <span class="md-title-col5">o</span>
+    <span class="md-title-col6">o</span>
+    <span class="md-title-col7">N</span>
+    <span class="md-title-col8">o</span>
+    <span class="md-title-col9">t</span>
+    <span class="md-title-col10">e</span>
+    <span class="md-title-col11">s</span>
+  </v-card-title>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+v-card-title {
+  font-size: 25px;
+  /* margin-left: -700px;
+  padding-left: 80px; */
+}
+.md-title-col1 {
+  color: blue;
+}
+.md-title-col2 {
+  color: brown;
+}
+.md-title-col3 {
+  color: orange;
+}
+.md-title-col4 {
+  color: purple;
+}
+.md-title-col5 {
+  color: green;
+}
+.md-title-col6 {
+  color: lightcoral;
+}
+.md-title-col7 {
+  color: blue;
+}
+.md-title-col8 {
+  color: maroon;
+}
+.md-title-col9 {
+  color: lightskyblue;
+}
+.md-title-col10 {
+  color: red;
+}
+.md-title-col11 {
+  color: green;
+}
+</style>
