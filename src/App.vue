78d148042581bca660170c21486ffef9d4cdfbb5
aakash2809 <aakashrajak289@gmail.com>
2021-03-12 03:58:43 +0530

    refactor : some scss and remove unnecessary comments

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
   <div id="app">
     <router-view />
-    <!-- <register />
-    <login /> -->
   </div>
 </template>
 
 <script>
-/* import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-} */
-
-/* import register from "./pages/register";
-import login from "./pages/register"; */
 export default {
   name: "App",
   components: {},
@@ -29,8 +16,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 100px;
 }
 </style>
