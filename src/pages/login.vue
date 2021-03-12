cade8e4a2627b28083033f10950a25664e310b91
aakash2809 <aakashrajak289@gmail.com>
2021-03-12 17:26:41 +0530

    refactor : design login page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,8 +1,168 @@
+
+
 <template>
-  <div><h1>login page</h1></div>
+  <div>
+    <form novalidate class="md-layout jc-center" @submit.prevent="validateUser">
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+        <div class="md-layout md-gutter">
+          <div class="md-layout-item md-small-size-100">
+            <md-card-header>
+              <div class="md-title" v-bind:style="styleObject">
+                <Title />
+              </div>
+              <div class="md-title">Login Fundoo Account</div>
+            </md-card-header>
+
+            <md-card-content>
+              <md-field :class="getValidationClass('email')">
+                <label for="email">Email</label>
+                <md-input
+                  outline
+                  dense
+                  type="email"
+                  name="email"
+                  id="email"
+                  autocomplete="email"
+                  v-model="form.email"
+                  :disabled="sending"
+                />
+                <span class="md-error" v-if="!$v.form.email.required"
+                  >The email is required</span
+                >
+                <span class="md-error" v-else-if="!$v.form.email.email"
+                  >Invalid email</span
+                >
+              </md-field>
+
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-size-100 md-small-size-100">
+                  <md-field :class="getValidationClass('password')">
+                    <label for="password"> Password</label>
+                    <md-input
+                      name="password"
+                      type="password"
+                      id="password"
+                      v-model="form.password"
+                      :disabled="sending"
+                    />
+                    <span class="md-error" v-if="!$v.form.password.required"
+                      >The password is required</span
+                    >
+                    <span
+                      class="md-error"
+                      v-else-if="!$v.form.password.minlength"
+                      >Password should contain minimum 4 charecters
+                    </span>
+                  </md-field>
+                </div>
+              </div>
+            </md-card-content>
+            <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+            <md-card-content>
+              <md-card-actions>
+                <span>
+                  <router-link to="/register" class="route-link"
+                    >register in instead</router-link
+                  >
+                </span>
+
+                <v-spacer> </v-spacer>
+                <md-button
+                  type="submit"
+                  class="md-raised md-primary"
+                  :disabled="sending"
+                  >Login</md-button
+                >
+              </md-card-actions>
+            </md-card-content>
+          </div>
+        </div>
+      </md-card>
+    </form>
+  </div>
 </template>
+
 <script>
+import { validationMixin } from "vuelidate";
+import router from "../router";
+import { required, email } from "vuelidate/lib/validators";
+import Title from "../components/fundooTitle";
+
 export default {
-  name: "login",
+  name: "Login",
+  mixins: [validationMixin],
+  data: () => ({
+    form: {
+      email: null,
+      password: null,
+    },
+    userSaved: false,
+    sending: false,
+  }),
+  validations: {
+    form: {
+      firstName: {
+        required,
+      },
+      lastName: {
+        required,
+      },
+      email: {
+        required,
+        email,
+      },
+
+      password: {
+        required,
+      },
+      cpassword: {
+        required,
+      },
+    },
+  },
+  methods: {
+    getValidationClass(fieldName) {
+      const field = this.$v.form[fieldName];
+      if (field) {
+        return {
+          "md-invalid": field.$invalid && field.$dirty,
+        };
+      }
+    },
+    clearForm() {
+      this.$v.$reset();
+      this.form.email = null;
+      this.form.password = null;
+      window.setTimeout(() => {
+        router.push({
+          name: "home",
+        });
+      }, 2000);
+    },
+    LoginUser() {
+      this.sending = true;
+      let data = {
+        email: this.form.email,
+        password: this.form.password,
+      };
+      console.log("signup details: ", data);
+    },
+
+    validateUser() {
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        this.saveUser();
+      }
+    },
+  },
+
+  components: {
+    Title,
+  },
 };
 </script>
+
+<style scoped>
+@import url("../scss/register.scss");
+</style>
