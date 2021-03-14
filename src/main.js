d5361447b045ffffe26f4cb109219afaf4ce6662
aakash2809 <aakashrajak289@gmail.com>
2021-03-14 13:50:16 +0530

    refactor : add baseURL for api call

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import router from './router/index.js'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-//import axios from 'axios'
+import axios from 'axios'
 
 Vue.use(router);
-/* import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-Vue.use(Vuetify) */
-//axios.defaults.baseURL = 'http://localhost:3000'
+
+axios.defaults.baseURL = 'http://localhost:3000'
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
 new Vue({
