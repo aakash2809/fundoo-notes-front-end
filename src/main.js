26309c3b29056c87003e94885a8ed472b5808b4e
aakash2809 <aakashrajak289@gmail.com>
2021-03-15 23:03:20 +0530

    feat : add separte file to config base url and common header

--- src/main.js
+++ src/main.js
@@ -4,17 +4,15 @@ import router from './router/index.js'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-import axios from 'axios'
+import './services/axios.config'
 
 Vue.use(router);
 
-axios.defaults.baseURL = 'http://localhost:3000'
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
 new Vue({
   router,
   render: h => h(App),
-
 }).$mount('#app')
 
 
