b4f20abe269ff93c0d1ed54dbfc08952499c7082
aakash2809 <aakashrajak289@gmail.com>
2021-03-15 23:16:06 +0530

    feat : add home component and dashboard page

---
+++ src/components/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>hello {{ name }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+  props: ["name"],
+};
+</script>
---
+++ src/pages/dashBoard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Home v-bind:name="firstName" />
+  </div>
+</template>
+<script>
+import Home from "../components/home.vue";
+export default {
+  name: "dashBoard",
+  components: {
+    Home,
+  },
+  data() {
+    return { firstName: `${localStorage.getItem("firstName")}` };
+  },
+};
+</script>
