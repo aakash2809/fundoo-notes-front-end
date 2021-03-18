5607fce3accdea84e8ec499e5b8c4724c86f9674
aakash2809 <aakashrajak289@gmail.com>
2021-03-18 11:43:04 +0530

    refactor : add header (drawer,serach box,icon)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -10,13 +12,12 @@ export default {
   components: {},
 };
 </script>
-
 <style lang ="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  padding-top: 100px;
+  /* padding-top: 100px; */
 }
 </style>
