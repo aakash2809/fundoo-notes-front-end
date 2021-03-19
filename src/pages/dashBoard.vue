0fd02113b19187f1f398d32db06c19bcb8ee5d9b
aakash2809 <aakashrajak289@gmail.com>
2021-03-19 08:32:37 +0530

    refactor : add side Navigation

--- src/pages/dashBoard.vue
+++ src/pages/dashBoard.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-    <Home v-bind:name="firstName" />
+    <div>
+      <Home v-bind:name="firstName" />
+    </div>
+    <div>
+      <SideNav />
+    </div>
   </div>
 </template>
 <script>
-import Home from "../components/home.vue";
+import Home from "../components/header.vue";
+import SideNav from "../components/sideNav.vue";
 export default {
   name: "dashBoard",
   components: {
     Home,
+    SideNav,
   },
   data() {
     return { firstName: `${localStorage.getItem("firstName")}` };
