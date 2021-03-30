9d99c47cf0786c423ab584c81609e618a2648107
aakash2809 <aakashrajak289@gmail.com>
2021-03-31 02:07:19 +0530

    refactor : remove unncessary comments and add filteration for archive notes

--- src/components/archive.vue
+++ src/components/archive.vue
@@ -1,11 +1,72 @@
 <template>
-  <div><h1>Archive Page</h1></div>
+  <v-flex>
+    <v-layout class="noteCards" row wrap>
+      <v-flex
+        v-for="note in archievedNotes"
+        v-bind:key="note._id"
+        md3
+        class="mr-5 mb-5"
+      >
+        <v-hover v-slot="{ hover }">
+          <v-card
+            class="mx-auto singleCard card-container"
+            outlined
+            :class="{ 'on-hover': hover }"
+          >
+            <article class="text-md-right text-lg-right pt-5 pr-4">
+              <v-icon v-bind="attrs" v-on="on" v-show="hover"
+                >mdi-pin-outline</v-icon
+              >
+            </article>
+            <v-card-title>{{ note.title }} </v-card-title>
+            <v-list-item>{{ note.description }}</v-list-item>
+            <v-list-item></v-list-item>
+            <cardIcons v-show="hover == true || click == true" />
+            <v-list-item></v-list-item>
+          </v-card>
+        </v-hover>
+      </v-flex>
+    </v-layout>
+  </v-flex>
 </template>
 
 <script>
+import userServices from "../services/user";
+
 export default {
-  name: "Archive",
-  components: {},
-  data: () => ({}),
+  name: "Archieved",
+
+  data: () => ({
+    allNotes: "",
+    archievedNotes: [],
+    result: "",
+    dialog: false,
+  }),
+
+  mounted() {
+    this.displayAllNotes();
+  },
+
+  methods: {
+    displayAllNotes() {
+      return userServices
+        .fetchAllNotes()
+        .then((result) => {
+          this.result = result.data.data;
+          console.log("data.data", this.result);
+          this.allNotes = [...this.result].reverse();
+          console.log(
+            "extration",
+            this.allNotes.find((note) => note.isArchived === true)
+          );
+          this.archievedNotes = this.allNotes.filter(
+            (note) => note.isArchived == true
+          );
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
