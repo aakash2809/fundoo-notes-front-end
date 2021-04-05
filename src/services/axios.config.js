88c7053aeab98d22ccd846434c30c5cd1e033b41
aakash2809 <aakashrajak289@gmail.com>
2021-04-05 09:30:54 +0530

    refactor : remove extra spaces

--- src/services/axios.config.js
+++ src/services/axios.config.js
@@ -1,9 +1,7 @@
 
 import axios from 'axios';
-//require("dotenv").config();
-//console.log(process.env.BASE_URL)
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
-//axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+
