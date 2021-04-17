690280b1bd0b503f17ecb77b131bcca1b9be0ba7
aakash2809 <aakashrajak289@gmail.com>
2021-04-18 01:31:11 +0530

    add : test archive component

---
+++ tests/unit/archieved.spec.js
@@ -0,0 +1,43 @@
+/*************************************************************************
+ * @file : archive.spec.js
+ * @author :  Aakash Rajak<aakashrajak2809@gmail.com>
+ * @since : 
+ **************************************************************************/
+
+import Archieved from '../../src/components/archive.vue';
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('archieved.vue', () => {
+    let getters
+    let store
+
+    beforeEach(() => {
+
+        getters = {
+            allArchievedNotes: () => [{
+                _id: ObjectId("6079fcca7b125b185c501879"),
+                labelId: [],
+                isArchived: false,
+                isDeleted: true,
+                collaborator: [],
+                title: "test second ",
+                description: "to check",
+                userId: ObjectId("604ebbc14711870344436aaa"),
+                createdAt: ISODate("2021-04-16T21:08:26.479Z"),
+                updatedAt: ISODate("2021-04-16T21:08:47.521Z")
+            }]
+        }
+        store = new Vuex.Store({
+            getters
+        })
+    })
+
+    it('mounts and renders', () => {
+        const wrapper = shallowMount(Archieved, { store, localVue })
+        expect(wrapper.html()).toBeTruthy();
+    });
+})
